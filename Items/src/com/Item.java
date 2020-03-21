cbce983d1d1b0e723ed21bde3d22c12b0ee50c63
chathu96 <chathuryaatthanayake2015@gmail.com>
2020-03-21 22:13:55 +0530

    First Commit

---
+++ Items/src/com/Item.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Item {
+	
+	public Connection connect()
+	{
+	 Connection con = null;
+
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+	 con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test",
+	 "root", "");
+	 //For testing
+	 System.out.print("Successfully connected");
+	 }
+	 catch(Exception e)
+	 {
+	 e.printStackTrace();
+	 }
+
+	 return con;
+	}
+
+	
+}
+
+
+
+
+
+
+
